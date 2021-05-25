55d36e3948d4d9e645aec1be7c7afa57e0e0e11d
RalfSasse <82031930+RalfSasse@users.noreply.github.com>
2021-05-25 21:28:06 +0200

    Update CustomAction.cs

--- DotNetCoreTest/CustomAction.cs
+++ DotNetCoreTest/CustomAction.cs
@@ -1,6 +1,6 @@
 /******************************************************************************
  * CustomAction.cs
- * Projekt DotNetCoreTest / CustomAction für WiX
+ * Projekt DotNetCoreTest / CustomAction fÃ¼r WiX
  * Datum: 19.05.2021
  * Autor: Ralf Sasse
  * Quelle: https://stackoverflow.com/questions/58930065/check-if-netcore-is-installed-using-customaction-with-wix
@@ -10,16 +10,16 @@
 using Microsoft.Deployment.WindowsInstaller;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;       // für Objekte der Klassen Process und ProcessStartInfo
+using System.Diagnostics;       // fÃ¼r Objekte der Klassen Process und ProcessStartInfo
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace FrameworkTest
+namespace DotNetCoreTestTest
 {
     public class CustomActions
     {
         // Definition einer Liste von Strings, in denen die zu suchenden Runtimes gespeichert werden.
-        // Tatsächlich wird in diesem Programm aber nur der erste Eintrag "runtimes[0]" benutzt,
+        // TatsÃ¤chlich wird in diesem Programm aber nur der erste Eintrag "runtimes[0]" benutzt,
         // also "Microsoft.NETCore.App" (siehe unten).
 
         static readonly List<string> runtimes = new List<string>()
@@ -30,28 +30,28 @@ namespace FrameworkTest
         };
 
         [CustomAction]
-        public static ActionResult FrameworkTest(Session session)
+        public static ActionResult DotNetCoreTest(Session session)
         {
             // Definition der Variablen
 
             // Mindest-Versionsnummer von .NET Core
 
-            var minVersionDotNetCore = new Version(3, 1, 14);
+            var minVersion = new Version(3, 1, 14);
 
-            // String für den Programm-Aufruf an der Kommandozeile, hier also
+            // String fÃ¼r den Programm-Aufruf an der Kommandozeile, hier also
             //   cmd /c dotnet --list-runtimes
             // Der Parameter /c ist wichtig - dadurch wird die Kommandozeile nach der
-            // Ausführung des dotnet-Kommandos automatisch wieder beendet.
+            // AusfÃ¼hrung des dotnet-Kommandos automatisch wieder beendet.
 
             var command = "/c dotnet --list-runtimes";
 
-            // Leerer String für die Ausgabe
+            // Leerer String fÃ¼r die Ausgabe
 
             var output = string.Empty;
 
             // An dieser Stelle wird der Start des Kommandozeilen-Prozesses vorbereitet:
             // Ein Objekt der Klasse Process wird erzeugt, das als Eigenschaft u.a. ein
-            // Objekt der Klasse ProcessStartInfo besitzt. Dieses wird mit den gewünschten
+            // Objekt der Klasse ProcessStartInfo besitzt. Dieses wird mit den gewÃ¼nschten
             // Werten initialisiert.
 
             using (var p = new Process())
@@ -71,9 +71,9 @@ namespace FrameworkTest
                 // Die Ausgabe des Kommandozeilen-Programms "dotnet --list-runtimes" besteht
                 // (normalerweise) aus mehreren Zeilen, die jetzt innerhalb einer Schleife
                 // in die String-Variable "output" eingelesen werden, jeweils gefolgt von einem
-                // Zeilenumbruch. Die Eigenschaft "NewLine" der Klasse "Environment" enthält das
-                // (bzw. die) Steuerzeichen für den Zeilenumbruch, abhängig vom Betriebssystem:
-                // "\r\n" für Windows, "\n" für Linux und MacOSX oder "\r" für ältere MacOS-Versionen.
+                // Zeilenumbruch. Die Eigenschaft "NewLine" der Klasse "Environment" enthÃ¤lt das
+                // (bzw. die) Steuerzeichen fÃ¼r den Zeilenumbruch, abhÃ¤ngig vom Betriebssystem:
+                // "\r\n" fÃ¼r Windows, "\n" fÃ¼r Linux und MacOSX oder "\r" fÃ¼r Ã¤ltere MacOS-Versionen.
 
                 while (!p.StandardOutput.EndOfStream)
                 {
@@ -82,10 +82,10 @@ namespace FrameworkTest
 
                 p.WaitForExit();    // Warten auf Beendigung des Prozesses
 
-                // Wenn der Prozess nicht erfolgreich beendet wurde (dann wäre der ExitCode = 0),
+                // Wenn der Prozess nicht erfolgreich beendet wurde (dann wÃ¤re der ExitCode = 0),
                 // dann wird die Session-Variable "DOTNETCORE3114" des WiX-Skripts auf Null gesetzt.
-                // Als Rückgabewert wird aber trotzdem "Success" zurückgegeben (an dieser Stelle
-                // könnte es sinnvoll sein, eine Exception abzufangen)
+                // Als RÃ¼ckgabewert wird aber trotzdem "Success" zurÃ¼ckgegeben (an dieser Stelle
+                // kÃ¶nnte es sinnvoll sein, eine Exception abzufangen)
 
                 if (p.ExitCode != 0)
                 {
@@ -106,16 +106,16 @@ namespace FrameworkTest
 
         // Diese Methode parst den (meist mehrzeiligen) String, den das Kommandozeilen-Programm
         // "dotnet" ausgibt (und der jetzt im String "output" steht, der dieser Methode als
-        // String-Argument "runtimesList" übergeben wird) und sucht darin die Versionsnummer
-        // der neuesten Version von .NET Core. Im ebenfalls übergebenen Argument "runtime"
+        // String-Argument "runtimesList" Ã¼bergeben wird) und sucht darin die Versionsnummer
+        // der neuesten Version von .NET Core. Im ebenfalls Ã¼bergebenen Argument "runtime"
         // steht nur der erste Listeneintrag "Microsoft.NETCore.App".
-        // Rückgabewert ist ein Objekt der Klasse "Version".
+        // RÃ¼ckgabewert ist ein Objekt der Klasse "Version".
 
         private static Version GetLatestVersionOfRuntime(string runtime, string runtimesList)
         {
             // An dieser Stelle wird der (wahrscheinlich mehrzeilige) String "runtimesList"
             // in mehrere Teilstrings gesplittet. Trennzeichen ist '\n' (nicht "Environment.NewLine",
-            // weil das ein String ist und kein char). Die Teilstrings werden in einer Art temporären
+            // weil das ein String ist und kein char). Die Teilstrings werden in einer Art temporÃ¤ren
             // Liste zwischengespeichert. Dort werden die Teilstrings gesucht, die den String "runtime"
             // enthalten (also "Microsoft.NETCore.App"), und diese werden alphanumerisch sortiert.
             // Im String "latestLine" steht am Ende der alphanumerisch letzte (bzw. der einzige)
@@ -123,8 +123,8 @@ namespace FrameworkTest
 
             var latestLine = runtimesList.Split('\n').ToList().Where(x => x.Contains(runtime)).OrderBy(x => x).LastOrDefault();
 
-            // Wenn in "runtimesList" keine Zeile enthalten war, die "Microsoft.NETCore.App" enthält,
-            // ist "latestLine" leer. Sonst wird die Versionsnummer mit Hilfe eines regulären Ausdrucks
+            // Wenn in "runtimesList" keine Zeile enthalten war, die "Microsoft.NETCore.App" enthÃ¤lt,
+            // ist "latestLine" leer. Sonst wird die Versionsnummer mit Hilfe eines regulÃ¤ren Ausdrucks
             // aus "latestLine" ermittelt.
 
             if (latestLine != null)
@@ -132,13 +132,13 @@ namespace FrameworkTest
                 // Ein Objekt der Klasse Regex (Regular Expression) wird erzeugt. Gesucht wird nach
                 // einem Teilstring, der aus mehreren durch Punkte voneinander getrennten Dezimalzahlen
                 // besteht. Das @-Zeichen vor dem String bewirkt, dass Sonderzeichen wie der Backslash
-                // nicht mehr maskiert werden müssen.
+                // nicht mehr maskiert werden mÃ¼ssen.
 
                 Regex pattern = new Regex(@"\d+(\.\d+)+");
 
-                // Ein Objekt der Klasse Match (sinngemäß "Treffer") wird erzeugt. Der in "pattern"
-                // gespeicherte reguläre Ausdruck wird auf den String "latestLine" angewandt.
-                // Rückgabewert ist die erste Übereinstimmung oder "Match.Empty".
+                // Ein Objekt der Klasse Match (sinngemÃ¤ÃŸ "Treffer") wird erzeugt. Der in "pattern"
+                // gespeicherte regulÃ¤re Ausdruck wird auf den String "latestLine" angewandt.
+                // RÃ¼ckgabewert ist die erste Ãœbereinstimmung oder "Match.Empty".
 
                 Match m = pattern.Match(latestLine);
 
@@ -148,7 +148,7 @@ namespace FrameworkTest
                 string versionValue = m.Value;
 
                 // Jetzt wird der String "version.Value" geparst und sein Wert in einem Objekt
-                // der Klasse "Version" gespeichert, das der Rückgabewert dieser Methode ist.
+                // der Klasse "Version" gespeichert, das der RÃ¼ckgabewert dieser Methode ist.
 
                 if (Version.TryParse(versionValue, out var version))
                 {
@@ -157,7 +157,7 @@ namespace FrameworkTest
             }
 
             return null;        // "latestLine" war Null; es wurde keine Zeile gefunden,
-                                // die den String "Microsoft.NETCore.App" enthält.
+                                // die den String "Microsoft.NETCore.App" enthÃ¤lt.
         }
     }
 }
